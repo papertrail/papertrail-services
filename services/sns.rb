dfbcd7f05f404a8d9a5a22c354e036e014e47c6f
James Burns <jfburns@gmail.com>
2014-06-24 14:44:59 -0700

    adding optional incident_id and more tests for config assertions

--- services/sns.rb
+++ services/sns.rb
@@ -5,16 +5,17 @@ class Service::SNS < Service
     raise_config_error 'Missing AWS Region' if settings[:aws_region].to_s.empty?
     raise_config_error 'Missing AWS SNS Topic' if settings[:aws_sns_topic_arn].to_s.empty?
 
-
     sns = AWS::SNS.new(
       :access_key_id => settings[:aws_access_key_id],
       :secret_access_key => settings[:aws_secret_access_key],
       :region => settings[:aws_region])
 
+    incident_id = settings[:aws_sns_incident_id] || ""
+
     topic = sns.topics[settings[:aws_sns_topic_arn]]
 
     payload[:events].each do |event|
-      topic.publish({ :default => event }.to_json)
+      topic.publish({ :default => event, :incident_id => incident_id }.to_json)
     end
   end
 end
--- test/sns_test.rb
+++ test/sns_test.rb
@@ -5,9 +5,34 @@ class SNSTest < PapertrailServices::TestCase
     AWS.stub!
   end
 
-  def test_logs
-    svc = service(:logs, { :aws_access_key_id => '1', :aws_secret_access_key => '2', :aws_region => '3', :aws_sns_topic_arn => 'arn:aws:sns:us-west-1:111111111111:pagerduty-test' }, payload)
+  def test_logs_no_incident_id
+    svc = service(:logs, { :aws_access_key_id => '1', :aws_secret_access_key => '2', :aws_region => '3', :aws_sns_topic_arn => 'arn:aws:sns:us-west-1:111111111111:papertrail-test' }, payload)
     svc.receive_logs
+  end
+
+  def test_logs_with_incident_id
+    svc = service(:logs, { :aws_access_key_id => '1', :aws_secret_access_key => '2', :aws_region => '3', :aws_sns_topic_arn => 'arn:aws:sns:us-west-1:111111111111:papertrail-test', :aws_sns_incident_id => 'test-id' }, payload)
+    svc.receive_logs
+  end
+
+  def test_no_access_id
+    svc = service(:logs, { :aws_secret_access_key => '2', :aws_region => '3', :aws_sns_topic_arn => 'arn:aws:sns:us-west-1:111111111111:papertrail-test' }, payload)
+    assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
+  end
+
+  def test_no_secret_access_key
+    svc = service(:logs, { :aws_access_key_id => '1', :aws_region => '3', :aws_sns_topic_arn => 'arn:aws:sns:us-west-1:111111111111:papertrail-test' }, payload)
+    assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
+  end
+
+  def test_no_region
+    svc = service(:logs, { :aws_access_key_id => '1', :aws_secret_access_key => '2', :aws_sns_topic_arn => 'arn:aws:sns:us-west-1:111111111111:papertrail-test' }, payload)
+    assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
+  end
+
+  def test_no_arn
+    svc = service(:logs, { :aws_access_key_id => '1', :aws_secret_access_key => '2', :aws_region => '3' }, payload)
+    assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
   end
 
   def service(*args)
