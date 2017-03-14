4c69250e3f03bba4f16f7c4ca0f1a6509b7ec530
Alexis Grant <alexisg@gmail.com>
2017-03-14 14:36:48 -0700

    Add tests for tag parsing

--- test/datadog_test.rb
+++ test/datadog_test.rb
@@ -13,9 +13,39 @@ class DatadogTest < PapertrailServices::TestCase
   end
 
   def test_logs
-    svc = service(:logs, { 'api_key' => 'foobar', "metric" => "foo.bar"}.with_indifferent_access, payload)
+    svc = service(:logs, { 'api_key' => 'foobar', "metric" => "foo.bar" }.with_indifferent_access, payload)
 
     http_stubs.post "/api/v1/series" do |env|
+      body = JSON(env[:body])
+
+      [200, {}, ""]
+    end
+
+    svc.receive_logs
+  end
+
+  def test_tagged_logs
+    svc = service(:logs, { 'api_key' => 'foobar', "metric" => "foo.bar", 'tags' => 'environment:production,sender:papertrail' }.with_indifferent_access, payload)
+
+    http_stubs.post "/api/v1/series" do |env|
+      body = JSON(env[:body])
+
+      assert body['series'][0]['tags'][0] == 'environment:production'
+
+      [200, {}, ""]
+    end
+
+    svc.receive_logs
+  end
+
+  def test_space_tagged_logs
+    svc = service(:logs, { 'api_key' => 'foobar', "metric" => "foo.bar", 'tags' => 'environment:staging, sender:papertrail' }.with_indifferent_access, payload)
+
+    http_stubs.post "/api/v1/series" do |env|
+      body = JSON(env[:body])
+
+      assert body['series'][0]['tags'][0] == 'environment:staging'
+
       [200, {}, ""]
     end
 
