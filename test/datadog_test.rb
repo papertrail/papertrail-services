e9e43c8d04c58890fa465eb76abd65dac5fbe574
Alexis Grant <alexisg@gmail.com>
2017-03-29 17:00:42 -0700

    even better resilience to comma formatting

--- test/datadog_test.rb
+++ test/datadog_test.rb
@@ -24,14 +24,15 @@ class DatadogTest < PapertrailServices::TestCase
   end
 
   def test_tagged_logs
-    svc = service(:logs, { 'api_key' => 'foobar', "metric" => "foo.bar", 'tags' => 'environment:production,sender:papertrail, type:error' }.with_indifferent_access, payload)
+    svc = service(:logs, { 'api_key' => 'foobar', "metric" => "foo.bar", 'tags' => 'environment:production,sender:papertrail, severity:error ,type:NoMethodError' }.with_indifferent_access, payload)
 
     http_stubs.post "/api/v1/series" do |env|
       body = JSON(env[:body])
 
       assert body['series'][0]['tags'][0] == 'environment:production'
       assert body['series'][0]['tags'][1] == 'sender:papertrail'
-      assert body['series'][0]['tags'][2] == 'type:error'
+      assert body['series'][0]['tags'][2] == 'severity:error'
+      assert body['series'][0]['tags'][3] == 'type:NoMethodError'
 
       [200, {}, ""]
     end
