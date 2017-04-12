b6053f81cbe64713e5cc31768962ab0a94809351
Alexis Grant <alexisg@gmail.com>
2017-04-12 12:18:39 -0700

    initial NR Insights alert integration

---
+++ services/new_relic.rb
@@ -0,0 +1,35 @@
+class Service::NewRelic < Service
+  def receive_logs
+    raise_config_error 'Missing account ID' if settings[:account_id].to_s.empty?
+    raise_config_error 'Missing API key' if settings[:insights_api_key].to_s.empty?
+    
+    post_url = "https://insights-collector.newrelic.com/v1/accounts/#{settings[:account_id]}/events"
+    http.headers = {
+        'Content-Type' => 'application/json'
+        'X-Insert-Key' => "#{settings[:insights_api_key]}"
+      }
+    
+    response = http_post post_url, (format_events(payload[:events])).to_json
+
+    unless response.success?
+      puts "new_relic: #{payload[:saved_search][:id]}: #{response.status}: #{response.body}"
+      raise_config_error "Could not submit log events to New Relic Insights"
+    end
+  end
+  
+  def format_events(events)
+    events.each do |event|
+      
+      # Set an event type (table name) # TODO: make these configurable?
+      event[:eventType] = 'PapertrailAlert'
+      # Give the event a name corresponding to the saved search (in case there are multiple alerts sending)
+      event[:search_name] = payload[:saved_search][:name]
+      
+      # Format the attributes so Insights handles them properly/doesn't reject them: see
+      # https://docs.newrelic.com/docs/insights/explore-data/custom-events/insert-custom-events-insights-api#limits
+      
+      event[:received_at] = Time.iso8601(event[:received_at]).to_i
+      event[:message] = event[:message].truncate(4000, separator: /\s/)
+    end
+  end
+end
---
+++ test/new_relic_test.rb
@@ -0,0 +1,35 @@
+require File.expand_path('../helper', __FILE__)
+
+class NewRelicTest < PapertrailServices::TestCase
+  def test_config
+    svc = service(:logs, {}.with_indifferent_access, payload)
+    assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
+
+    svc = service(:logs, { 'insights_api_key' => "foobar" }.with_indifferent_access, payload)
+    assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
+
+    svc = service(:logs, { 'account_id' => "1929219" }.with_indifferent_access, payload)
+    assert_raises(PapertrailServices::Service::ConfigurationError) { svc.receive_logs }
+  end
+  
+  def test_logs
+    svc = service(:logs, { 'insights_api_key' => '9872--3042dtshN3oen', "account_id" => "531007"}.with_indifferent_access, payload)
+
+    http_stubs.post "/v1/accounts/#{settings[:account_id]}/events" do |env|
+      [200, {}, ""]
+    end
+
+    svc.receive_logs
+  end
+  
+  # TODO: Other things to test
+  # A formatted event contains an eventType and search_name key 
+  # A formatted event's received_at is in UNIX epoch
+  # A message longer than 4K is truncated
+  # If the payload has > 1000 events, handle that case
+  
+  def service(*args)
+    super Service::NewRelic, *args
+  end
+  
+end
