6bae5284282be59ffee7f2d26b145ec7830c619d
Cory Duncan <cory@librato.com>
2017-08-07 16:09:24 -0700

    Fix Pushover for inactivity alerts

--- services/pushover.rb
+++ services/pushover.rb
@@ -3,26 +3,8 @@
 class Service::Pushover < Service
   attr_writer :pushover
 
-  def receive_logs
-    raise_config_error 'Missing pushover app token' if
-      settings[:token].to_s.empty?
-    raise_config_error 'Missing pushover user token' if
-      settings[:user_key].to_s.empty?
-
-    events = payload[:events]
-
-    hosts = events.collect { |e| e[:source_name] }.sort.uniq
-    title = payload[:saved_search][:name]
-    if hosts.length < 5
-        title = "#{title} (#{hosts.join(', ')})"
-      else
-        title = "#{title} (from #{hosts.length} hosts)"
-    end
-
-    message = events.collect { |item|
-      syslog_format(item)
-    }
-
+  def post_data(body)
+    message = body[:message]
     message = message[0..1020] + "..." if message.length > 1024
 
     if message.empty?
@@ -31,15 +13,60 @@ class Service::Pushover < Service
 
     resp = pushover.notify(settings[:user_key],
                            message,
-                           {title: title,
-                            timestamp: Time.iso8601(events[0][:received_at]).to_i,
-                            url: payload[:saved_search][:html_search_url],
+                           {title: body[:title],
+                            timestamp: body[:timestamp].to_i,
+                            url: body[:search_url],
                             url_title: "View logs on Papertrail"})
 
     unless resp.ok?
       puts "pushover: #{resp.to_s}"
 
       raise_config_error "Failed to post to Pushover"
+    end
+  end
+
+  def receive_logs
+    raise_config_error 'Missing pushover app token' if
+      settings[:token].to_s.empty?
+    raise_config_error 'Missing pushover user token' if
+      settings[:user_key].to_s.empty?
+
+    events = payload[:events]
+    title  = payload[:saved_search][:name]
+
+    if events.present?
+      hosts = events.collect { |e| e[:source_name] }.sort.uniq
+
+      if hosts.length < 5
+        title = "#{title} (#{hosts.join(', ')})"
+      else
+        title = "#{title} (from #{hosts.length} hosts)"
+      end
+
+      message = events.collect { |item|
+        syslog_format(item)
+      }
+
+      body = {
+        :title => title,
+        :message => message,
+        :timestamp => Time.iso8601(events[0][:received_at]),
+        :search_url => payload[:saved_search][:html_search_url]
+      }
+
+      post_data(body)
+    else
+      frequency = frequency_phrase(payload[:frequency])
+      message   = %{0 matches found #{frequency}}
+
+      body = {
+        :title => title,
+        :message => message,
+        :timestamp => Time.now,
+        :search_url => payload[:saved_search][:html_search_url]
+      }
+
+      post_data(body)
     end
   end
 
