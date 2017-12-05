1e44ce6cb3bc83a5b8544ce3dd9dc9964276a111
Alexis Grant <alexisg@gmail.com>
2017-12-05 14:19:29 -0800

    proper requires...

--- services/appoptics.rb
+++ services/appoptics.rb
@@ -1,16 +1,16 @@
 # encoding: utf-8
 require 'appoptics/metrics/processor'
-require 'metrics/aggregator'
-require 'metrics/annotator'
-require 'metrics/client'
-require 'metrics/collection'
-require 'metrics/connection'
-require 'metrics/errors'
-require 'metrics/persistence'
-require 'metrics/queue'
-require 'metrics/smart_json'
-require 'metrics/util'
-require 'metrics/version'
+require 'appoptics/metrics/aggregator'
+require 'appoptics/metrics/annotator'
+require 'appoptics/metrics/client'
+require 'appoptics/metrics/collection'
+require 'appoptics/metrics/connection'
+require 'appoptics/metrics/errors'
+require 'appoptics/metrics/persistence'
+require 'appoptics/metrics/queue'
+require 'appoptics/metrics/smart_json'
+require 'appoptics/metrics/util'
+require 'appoptics/metrics/version'
 
 
 class Service::AppOptics < Service  
