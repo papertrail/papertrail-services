e5fd1fbeb45e9be422001de8650bb0d0f0b66b2d
Joey Nelson <joey@wlrs.net>
2019-04-26 08:07:27 -0400

    test root path

--- test/app_test.rb
+++ test/app_test.rb
@@ -8,6 +8,12 @@ class AppTest < Minitest::Test
     PapertrailServices::App
   end
 
+  def test_root
+    get '/'
+    assert last_response.ok?
+    assert_equal 'ok', last_response.body
+  end
+
   def test_post_event
     Service::AppOptics.any_instance.expects(:receive_logs)
     $stdout.expects(:puts) # keep test output clean
