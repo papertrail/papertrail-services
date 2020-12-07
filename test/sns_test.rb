b909fd278b6f48fec781482b9eba0b2daa55c929
Stas Dulman <stas.dulman@samanage.com>
2020-12-07 09:58:04 +0200

    test fails

--- test/sns_test.rb
+++ test/sns_test.rb
@@ -1,5 +1,5 @@
 require File.expand_path('../helper', __FILE__)
-ENV['STUB_RESPONSES'] = 'true'
+# ENV['STUB_RESPONSES'] = 'true'
 class SNSTest < PapertrailServices::TestCase
   def test_logs
     svc = service(:logs, { :aws_access_key_id => '1', :aws_secret_access_key => '2', :aws_region => 'us-east-1', :aws_sns_topic_arn => 'arn:aws:sns:us-east-1:111111111111:pagerduty-test' }, payload)
